c349868783adc2f5f1c8d8bcc8864e23d2d1c12c
okcabroad <okcabroad@gmail.com>
2024-03-19 22:30:12 +0900

    random dice game

---
+++ Dice-Game /script.js
@@ -0,0 +1,8 @@
+const diceresult = document.getElementById('diceresult');
+const rollDice = document.getElementById('rollDice');
+const reset = document.getElementById('reset');
+
+function rollDice() {
+    const dice = Math.floor(Math.random() * 6 + 1);
+    diceres
+}
