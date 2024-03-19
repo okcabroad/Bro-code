45008a4988e7bf42649fe96d2b9ec537b0e04b8b
okcabroad <okcabroad@gmail.com>
2024-03-19 22:47:26 +0900

    added dice functions

--- Dice-Game /script.js
+++ Dice-Game /script.js
@@ -1,8 +1,33 @@
-const diceresult = document.getElementById('diceresult');
-const rollDice = document.getElementById('rollDice');
-const reset = document.getElementById('reset');
-
-function rollDice() {
+function rollDiceFunction() {
+    const diceresult = document.getElementById('diceresult');
     const dice = Math.floor(Math.random() * 6 + 1);
-    diceres
+    switchDice(dice, diceresult);
+}
+
+function switchDice(dice, diceresult) {
+    switch(dice) {
+        case 1:
+            diceresult.src = '1dice.svg';
+            break;
+        case 2: 
+            diceresult.src = '2dice.svg';
+            break;
+        case 3:
+            diceresult.src = '3dice.svg';
+            break;
+        case 4:
+            diceresult.src = '4dice.svg';
+            break;
+        case 5:
+            diceresult.src = '5dice.svg';
+            break;
+        case 6:
+            diceresult.src = '6dice.svg';
+            break;    
+    }
+}
+
+function reset() {
+    const diceresult = document.getElementById('diceresult');
+    diceresult.src = '1dice.svg'; // Reset to initial dice image
 }
